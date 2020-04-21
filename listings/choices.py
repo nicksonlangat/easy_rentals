995d45b1f95271f430089ebd2c452b71f6dc6743
LoyalFlames <nicksonlangat95@gmail.com>
2020-04-21 17:14:24 +0300

    initial commit

---
+++ listings/choices.py
@@ -0,0 +1,30 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+
+  }
+
+price_choices = {
+  '2000':'kshs5000',
+  '6000':'kshs10000',
+  '11000':'kshs20,000',
+  
+}
+
+location_choices = {
+        
+       'AP': 'Longisa ',
+       'HYD' : 'Mulot' ,
+       'AP' : 'Kapsuser',
+       'AS':'Chebarus',
+       'BH':'Silibwet',
+       'P':'Chebole',
+       'CHG':'Chemaner',
+       'Rp':'Siwot',
+       'G': 'Tenwek',
+       
+       
+}
